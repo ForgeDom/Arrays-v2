049a8460cd2aaa15db16fb19857c39dd56facc45
ForgeDom <maxstill2006@gmail.com>
2024-05-23 19:06:37 +0300

    lab ex1 done

--- main.cpp
+++ main.cpp
@@ -3,6 +3,28 @@
 using namespace std;
 
 int main() {
-
+	srand(time(0));
+	const int size = 10;
+	int arr[size];
+	
+	int index = 0;
+	for (int i = 0; i < size; i++) {
+		arr[i] = rand() % 10 - 5;
+	}
+	for (int i = 0; i < size; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+	for (int i = 0; i < size; ++i) {
+		if (arr[i] != 0) {
+			arr[index++] = arr[i];
+		}
+	}
+	for (int i = index; i < size; ++i) {
+		arr[i] = -1;
+	}
+	for (int i = 0; i < size; i++) {
+		cout << arr[i] << " ";
+	}
 	return 0;
 }

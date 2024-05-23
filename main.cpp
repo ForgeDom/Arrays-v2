381cc9dd50d187b62139761ac3aced78a84ff540
ForgeDom <maxstill2006@gmail.com>
2024-05-23 20:34:16 +0300

    dop1 done

--- main.cpp
+++ main.cpp
@@ -3,6 +3,32 @@
 using namespace std;
 
 int main() {
+    const int size = 10;
+    int arr[size];
 
-	return 0;
+    srand(time(0));
+
+    for (int i = 0; i < size; ++i) {
+        arr[i] = rand() % 10;
+    }
+    for (int i = 0; i < size; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    bool foundDuplicates = false;
+
+    for (int i = 0; i < size; ++i) {
+        for (int j = i + 1; j < size; ++j) {
+            if (arr[i] == arr[j]) {
+                if (!foundDuplicates) {
+                    foundDuplicates = true;
+                }
+                cout << arr[i] << " ";
+                break;
+            }
+        }
+    }
+
+    return 0;
 }
